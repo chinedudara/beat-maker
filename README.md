3bbc9bae22ba2b8586b0b19d255f9e3c609a473d
Chinedu Dara <darachinedu@gmail.com>
2019-05-19 14:49:14 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Beat Maker :notes:
+<br/>
+
+> Make music by tapping pads and make colorful animation while tapping :smile:. 
+
+I built this using vanilla javascript by following a tutorial by [Dev Ed]('https://youtu.be/2VJlzeEVL8A')  
+
+---  
+**Future Considerations**
+
+* [ ] Make this into a react app
+* [ ] Add more beat samples
+* [ ] Record the produced beats :wink:
+* [ ] Make the color balls of random sizes and move in random directions :stuck_out_tongue_closed_eyes:
---
+++ index.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', () => {
+    const sounds = document.querySelectorAll('.sound');
+    const pads = document.querySelectorAll('.pads div');
+    const visual = document.querySelector('.visual');
+    const colors = ['#60d394', '#eb733c', '#c060d3', '#d3d160', '#f05c5c', '#29d0ee']
+
+    // Sound on!
+    pads.forEach((pad, index) => {
+        pad.addEventListener('click', function (){
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+            createBubbles(index);
+        })
+    })
+
+    // Now some visuals!
+    const createBubbles = (index) => {
+        const bubble = document.createElement('div');
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = 'jump 1s ease'
+        bubble.addEventListener('animationend', function(){
+            visual.removeChild(this);
+        })
+    }
+})
+
